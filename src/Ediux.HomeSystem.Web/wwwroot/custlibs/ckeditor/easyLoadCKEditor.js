b71a68b5023f92af3072ea1a5e7637309a114688
HOME\ediux <ediuxcity@gamil.com>
2021-10-24 10:57:48 +0800

    更新CKEditor

---
+++ src/Ediux.HomeSystem.Web/wwwroot/custlibs/ckeditor/easyLoadCKEditor.js
@@ -0,0 +1,82 @@
+﻿(function ($) {
+	var currLCID = abp.localization.currentCulture;
+	var loc = currLCID.cultureName;
+
+	if (loc === "zh-Hant") {
+		loc = 'zh';
+	} else {
+		if (loc.cultureName === "zh-Hans") {
+			loc = 'zh-cn';
+		}
+	}
+
+    $.fn.CKEditor = function ($options) {
+		var l = abp.localization.getResource('HomeSystem');
+		var options = $.extend({
+			toolbar: {
+				items: [
+					'heading',
+					'highlight',
+					'|',
+					'bold',
+					'italic',
+					'link',
+					'bulletedList',
+					'numberedList',
+					'fontBackgroundColor',
+					'fontColor',
+					'fontFamily',
+					'fontSize',
+					'|',
+					'outdent',
+					'indent',
+					'|',
+					'alignment',
+					'|',
+					'imageUpload',
+					'blockQuote',
+					'insertTable',
+					'mediaEmbed',
+					'imageInsert',
+					'undo',
+					'redo',
+					'code',
+					'codeBlock',
+					'htmlEmbed',
+					'horizontalLine',
+					'|',
+					'findAndReplace'
+				]
+			},
+			language: loc,
+			image: {
+				toolbar: [
+					'imageTextAlternative',
+					'imageStyle:inline',
+					'imageStyle:block',
+					'imageStyle:side'
+				]
+			},
+			table: {
+				contentToolbar: [
+					'tableColumn',
+					'tableRow',
+					'mergeTableCells'
+				]
+			},
+			licenseKey: '',
+		}, $options);
+
+		var id = '#' + $(this).prop('id');
+		ClassicEditor
+			.create(document.querySelector(id), options)
+			.then(editor => {
+				editor.model.document.on('change:data', () => {
+					$(id).val(editor.getData());
+				});
+			})
+			.catch(error => {
+				abp.notify.error(error, l('Settings:WebSettingsGroupComponents'));
+			});
+    };
+})(jQuery);
