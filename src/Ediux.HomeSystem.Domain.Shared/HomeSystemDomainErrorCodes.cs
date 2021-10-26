a167ccee9ac6cfa3c82d6b72c18e0f2b4f53a93b
HOME\ediux <ediuxcity@gamil.com>
2021-10-27 04:13:01 +0800

    修正錯誤代碼

--- src/Ediux.HomeSystem.Domain.Shared/HomeSystemDomainErrorCodes.cs
+++ src/Ediux.HomeSystem.Domain.Shared/HomeSystemDomainErrorCodes.cs
@@ -5,7 +5,7 @@
         /* You can add your business exception error codes here, as constants */
         public const string GeneralError = "HomeSystem:99999";
         public const string DataAlreadyExistsError = "HomeSystem:99501";
-        public const string SpecifyDataItemAlreadyExistsError = "HomeSystem:94041";
+        public const string SpecifyDataItemAlreadyExistsError = "HomeSystem:99502";
         public const string DataNotFound = "HomeSystem:94040";
         public const string SpecifyDataItemNotFound = "HomeSystem:94041";
     }
