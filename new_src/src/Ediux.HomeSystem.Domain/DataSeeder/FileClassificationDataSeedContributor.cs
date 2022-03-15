5f6471370745cc8367c8bd60ccb4df5c8c2c734b
黃建豪 <ediux@msn.com>
2022-03-15 17:42:09 +0800

    檔案上傳

---
+++ new_src/src/Ediux.HomeSystem.Domain/DataSeeder/FileClassificationDataSeedContributor.cs
@@ -0,0 +1,38 @@
+﻿using Ediux.HomeSystem.SystemManagement;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.MultiTenancy;
+
+namespace Ediux.HomeSystem.DataSeeder
+{
+    public class FileClassificationDataSeedContributor : IDataSeedContributor, ITransientDependency
+    {
+        private readonly IRepository<FileStoreClassification, Guid> _fileStoreClassifications;
+        private readonly ICurrentTenant _currentTenant;
+
+        public FileClassificationDataSeedContributor(IRepository<FileStoreClassification, Guid> fileStoreClassifications,
+           ICurrentTenant currentTenant)
+        {
+            _fileStoreClassifications = fileStoreClassifications;
+            _currentTenant = currentTenant;
+        }
+        public async Task SeedAsync(DataSeedContext context)
+        {
+            using (_currentTenant.Change(context?.TenantId))
+            {
+                await _fileStoreClassifications.InsertAsync(new FileStoreClassification()
+                {
+                    Name = "Root"
+                });
+            }
+        }
+    }
+}
